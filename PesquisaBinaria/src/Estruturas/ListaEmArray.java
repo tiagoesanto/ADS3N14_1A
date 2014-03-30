234bd6536538d4344a873edca297b107db4a9d97
tiagoesanto <tiagoesanto@gmail.com>
2014-03-30 18:17:31 -0300

    Criado Método para pesquisa binária

--- PesquisaBinaria/src/Estruturas/ListaEmArray.java
+++ PesquisaBinaria/src/Estruturas/ListaEmArray.java
@@ -24,4 +24,46 @@ public class ListaEmArray {
 		}
 	}	
 
+	/**
+	 * Pesquisa no ArrayList utilizando um string inicial
+	 * @param ch string inicial
+	 * @return Nodo<Pessoa> pessoa encontrada, se estiver nulo, não encontrou nenhuma pessoa
+	 */
+	public Nodo<Pessoa> searchContact(String ch)
+	{
+		boolean finaliza = false;
+		int tamanhoDaLista = lista.size();
+		int posicaoAtual = (int)Math.floor(tamanhoDaLista/2);
+		Nodo<Pessoa> ret = null;
+		int metade = posicaoAtual;
+
+		while (!finaliza)
+		{
+			if (!lista.get(posicaoAtual).getChave().getNome().toUpperCase().startsWith(ch.toUpperCase()))
+			{
+				int comparar = lista.get(posicaoAtual).getChave().getNome().substring(0, 1).toUpperCase().compareTo(ch.toUpperCase());
+
+				metade /= 2;
+				if (comparar > 0 && metade >= 1)
+				{
+					posicaoAtual -= metade;
+				}
+				else if (comparar < 0 && metade >= 1)
+				{
+					posicaoAtual += metade;
+				}
+				else
+				{
+					finaliza = true;
+				}
+			}
+			else
+			{
+				ret = lista.get(posicaoAtual);
+				finaliza = true;
+			}
+		}
+
+		return ret;
+	}
 }
