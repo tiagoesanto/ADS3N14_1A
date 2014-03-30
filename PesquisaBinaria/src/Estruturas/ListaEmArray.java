9a603e35aba3ec218dcd3531da624c3108bfe006
tiagoesanto <tiagoesanto@gmail.com>
2014-03-30 18:31:15 -0300

    Criada propriedade que mostra o contador de comparações da ultima pesquisa binaria

--- PesquisaBinaria/src/Estruturas/ListaEmArray.java
+++ PesquisaBinaria/src/Estruturas/ListaEmArray.java
@@ -7,6 +7,9 @@ import Model.Pessoa;
 public class ListaEmArray {
 	
 	ArrayList<Nodo<Pessoa>> lista = new ArrayList<Nodo<Pessoa>>();
+	
+	// Propriedade que mostra o contador de comparações efetuadas na ultima pesquisa
+	int contadorUltimaPesquisa;
 
 	/**
 	 * Método Construtor
@@ -25,12 +28,13 @@ public class ListaEmArray {
 	}	
 
 	/**
-	 * Pesquisa no ArrayList utilizando um string inicial
-	 * @param ch string inicial
+	 * Pesquisa no ArrayList utilizando o nome
+	 * @param ch nome
 	 * @return Nodo<Pessoa> pessoa encontrada, se estiver nulo, não encontrou nenhuma pessoa
 	 */
-	public Nodo<Pessoa> searchContact(String ch)
+	public Nodo<Pessoa> procuraContato(String nome)
 	{
+		contadorUltimaPesquisa = 0;		
 		boolean finaliza = false;
 		int tamanhoDaLista = lista.size();
 		int posicaoAtual = (int)Math.floor(tamanhoDaLista/2);
@@ -39,9 +43,11 @@ public class ListaEmArray {
 
 		while (!finaliza)
 		{
-			if (!lista.get(posicaoAtual).getChave().getNome().toUpperCase().startsWith(ch.toUpperCase()))
+			contadorUltimaPesquisa ++;
+			
+			if (!lista.get(posicaoAtual).getChave().getNome().toUpperCase().startsWith(nome.toUpperCase()))
 			{
-				int comparar = lista.get(posicaoAtual).getChave().getNome().substring(0, 1).toUpperCase().compareTo(ch.toUpperCase());
+				int comparar = lista.get(posicaoAtual).getChave().getNome().substring(0, 1).toUpperCase().compareTo(nome.toUpperCase());
 
 				metade /= 2;
 				if (comparar > 0 && metade >= 1)
@@ -66,4 +72,6 @@ public class ListaEmArray {
 
 		return ret;
 	}
+	
+	
 }
